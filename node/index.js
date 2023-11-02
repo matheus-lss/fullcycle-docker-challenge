886e6a61dc5e1fe555561aeb19f44eeec922e27e
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2023-11-01 22:13:44 -0300

    chore: improve index.js

--- node/index.js
+++ node/index.js
@@ -1,24 +1,51 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const port = 3000;
 const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database:'nodedb'
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
 };
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Matheus')`
-connection.query(sql)
-connection.end()
+app.get("/", (req, res) => {
+  insertPeople(res);
+});
 
+app.listen(port, () => {
+  console.log("Rodando na porta " + port);
+});
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
-})
+async function insertPeople(res) {
+  const connection = mysql.createConnection(config);
+  const insert = `INSERT INTO people(name) VALUES 
+  ('Emerson'),
+  ('Maria'),
+  ('Everaldo');`;
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
-})
+  connection.query(insert);
+  console.log("Dados inserido no banco!");
+  getPeople(res, connection);
+}
+
+function getPeople(res, connection) {
+  const sql = `SELECT id, name FROM people`;
+
+  connection.query(sql, (error, results, fields) => {
+    if (error) {
+      throw error;
+    }
+
+    let table = "<table>";
+    table += "<tr><th>ID</th><th>Nome</th></tr>";
+    for (let people of results) {
+      table += `<tr><td>${people.id}</td><td>${people.name}</td></tr>`;
+    }
+
+    table += "</table>";
+    res.send("<h1>Full Cycle Rocks!</h1>" + table);
+  });
+
+  connection.end();
+}
